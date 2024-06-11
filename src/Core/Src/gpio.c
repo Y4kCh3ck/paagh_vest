4783fa5d0bbe9517cd7d5c36ed4ba58c06520fef
= <=>
2024-06-11 13:23:00 +0200

    added heater enabler

--- src/Core/Src/gpio.c
+++ src/Core/Src/gpio.c
@@ -53,7 +53,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, NRF24_CSN_Pin|USER_LED_Pin|I2C_EN_Pin|GPS_EN_Pin
-                          |ADC_EN_Pin, GPIO_PIN_RESET);
+                          |ADC_EN_Pin|HEATER_EN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(NRF24_CE_GPIO_Port, NRF24_CE_Pin, GPIO_PIN_RESET);
@@ -65,16 +65,16 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PAPin PAPin PAPin PAPin
-                           PAPin */
+                           PAPin PAPin */
   GPIO_InitStruct.Pin = NRF24_CSN_Pin|USER_LED_Pin|I2C_EN_Pin|GPS_EN_Pin
-                          |ADC_EN_Pin;
+                          |ADC_EN_Pin|HEATER_EN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA4 PA15 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_15;
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
   GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
